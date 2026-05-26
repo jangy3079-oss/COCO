a55f8f7aa01fb006774ecceddd8286b227fd4e39
vixiv0418 <kai2339@naver.com>
2026-05-26 23:33:52 +0900

    feat: initial gradle setup

---
+++ coco-backend/src/main/java/com/coco/backend/CocoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coco.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoBackendApplication.class, args);
+    }
+
+}
